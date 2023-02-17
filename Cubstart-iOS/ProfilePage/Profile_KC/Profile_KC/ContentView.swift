75e9304da1d125161a2c83038effeb520fcfd345
KC <kc.kim@berkeley.edu>
2023-02-16 17:37:58 -0800

    test

---
+++ Cubstart-iOS/ProfilePage/Profile_KC/Profile_KC/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Profile_KC
+//
+//  Created by Kyoungcheol Kim on 2/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //variables and functions
+    
+    var body: some View {
+        //all UI elements
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Cubstart-iOS/ProfilePage/Profile_KC/Profile_KC/Profile_KCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_KCApp.swift
+//  Profile_KC
+//
+//  Created by Kyoungcheol Kim on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Profile_KCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
