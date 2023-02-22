86915d9961189ec0060145238ea01c3f5222fde4
KC <kc.kim@berkeley.edu>
2023-02-21 16:28:55 -0800

    photos done

--- Cubstart-iOS/Profile_KC/Profile_KC/ContentView.swift
+++ Cubstart-iOS/Profile_KC/Profile_KC/ContentView.swift
@@ -9,14 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     //variables and functions
+    var name = "KC"
+    @State var num = 0
+    
+    func decrement(a:Int) -> Void {
+        if (num != 0) {
+            num -= 1
+        }
+    }
     
     var body: some View {
         //all UI elements
         
-        Text("Hello, world!").padding()
+        Image("hyochul")
+            .resizable().aspectRatio(contentMode: .fill)
+            .frame(width:200, height: 200)
+            .clipShape(Circle())
         
-        Button("Click me") {
-            print("Heyyyyy")
+        Text("KC Kim")
+            .padding()
+            .font(.system(size:50))
+        
+        HStack(spacing: 50) {
+            Image("Lucy1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width:150, height: 150)
+                .clipShape(Rectangle())
+            
+            Image("louie")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width:150, height: 150)
+                .clipShape(Rectangle())
+        }
+        
+        HStack(spacing: 50) {
+            Image("sleeplucy")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width:150, height: 150)
+                .clipShape(Rectangle())
+            
+            Image("ball")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width:150, height: 150)
+                .clipShape(Rectangle())
+        }
         }
     }
-}
+
